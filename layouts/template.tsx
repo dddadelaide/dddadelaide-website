bef05969a2095f05e316835ca39daad6169e3b57
Andrew Best <andrew.a.best@gmail.com>
2025-10-01 19:06:42 +0930

    Fixes issue with mailing list not displaying on navigate

--- layouts/template.tsx
+++ layouts/template.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import { Footer } from 'components/global/Footer/footer'
 import { Meta } from 'components/global/meta'
 import { SkipToContent } from 'components/SkipToContent/SkipToContent'
@@ -38,6 +38,28 @@ export const Template = ({
   const { conference, appConfig, dates } = useConfig()
   const menu = Menu(conference, dates)
 
+  useEffect(() => {
+    if (!hideMailingList) {
+      // Remove existing script if present to avoid duplicates
+      const existingScript = document.querySelector('script[data-form="78f46ea2-4b21-11f0-95a4-3de79b22cc0e"]')
+      if (existingScript) {
+        existingScript.remove()
+      }
+
+      // Create and append the script
+      const script = document.createElement('script')
+      script.src = 'https://eocampaign1.com/form/78f46ea2-4b21-11f0-95a4-3de79b22cc0e.js'
+      script.async = true
+      script.setAttribute('data-form', '78f46ea2-4b21-11f0-95a4-3de79b22cc0e')
+      document.getElementById('mailing-list-container')?.appendChild(script)
+
+      return () => {
+        // Cleanup: remove script on unmount
+        script.remove()
+      }
+    }
+  }, [hideMailingList])
+
   return (
     <Fragment>
       <Meta pageTitle={title} pageFullTitle={fullTitle} pageDescription={description} pageImage={image} />
@@ -53,11 +75,7 @@ export const Template = ({
       {!hideMailingList && (
         <div style={{ margin: '0 auto', textAlign: 'center', paddingBottom: '10px' }}>
           <h2>Mailing List</h2>
-          <script
-            async
-            src="https://eocampaign1.com/form/78f46ea2-4b21-11f0-95a4-3de79b22cc0e.js"
-            data-form="78f46ea2-4b21-11f0-95a4-3de79b22cc0e"
-          ></script>
+          <div id="mailing-list-container"></div>
         </div>
       )}
       <Footer />
